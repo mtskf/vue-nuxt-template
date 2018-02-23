5b710553fa77ff9364a7f46883750fe15f14fa0f
Luke Harback <luke@handbuiltcreative.com.au>
2018-02-23 15:47:52 +1100

    Renamed footer & menu to not use built-in or reserved HTML names

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -16,7 +16,7 @@
 <script>
 
 export default {
-  name: 'footer',
+  name: 'site-footer',
 }
 
 </script>
--- app/components/Menu.vue
+++ app/components/Menu.vue
@@ -16,7 +16,7 @@
 
 <script>
 export default {
-  name: 'menu',
+  name: 'site-menu',
 }
 </script>
 
